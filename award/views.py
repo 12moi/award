2cbd476ae4e8d8320e8187ae5d4d72c5201aaf97
12moi <12moi>
2022-04-08 18:22:54 +0300

    add user_profile method

--- award/views.py
+++ award/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import  PostForm
 from .models import  Post
+from django.contrib.auth import login, authenticate
+from .serializers import ProfileSerializer, UserSerializer, PostSerializer
+from django.contrib.auth.models import User
 import random
 
 # Create your views here.
@@ -29,3 +32,12 @@ def index(request):
 @login_required(login_url='login')
 def profile(request, username):
     return render(request, 'profile.html')
+
+def user_profile(request, username):
+    user_prof = get_object_or_404(User, username=username)
+    if request.user == user_prof:
+        return redirect('profile', username=request.user.username)
+    params = {
+        'user_prof': user_prof,
+    }
+    return render(request, 'userprofile.html', params)
